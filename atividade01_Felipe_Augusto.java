bbc3a4e65bb15e3d1856606708a555dbe4475eba
FelipeAugusto1208 <jocenirevangelistarocha@gmail.com>
2025-09-11 20:22:54 -0300

    Add files via upload

---
+++ atividade01_Felipe_Augusto.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        int d;
+
+        a = 25;
+        b = a * 3;
+        c = 10;
+        d = a / c;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+        String nome;
+        int idade;
+        double altura;
+        boolean estudante;
+
+        nome = "Felipe";
+        idade = 16;
+        altura = 1.75;
+        estudante = false;
+
+        System.out.println(nome);
+        System.out.println(idade);
+        System.out.println(altura);
+        System.out.println(estudante);
+
+    }
+}
