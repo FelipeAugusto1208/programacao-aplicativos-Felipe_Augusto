75c145c419b5016f6def4fc657929a3a9da3371e
FelipeAugusto1208 <jocenirevangelistarocha@gmail.com>
2025-09-16 21:01:10 -0300

    Add files via upload

---
+++ Atividade02_Felipe_Augusto.java
@@ -0,0 +1,178 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade02_alexandre_e_felipe;
+
+/**
+ *
+ * @author Aluno
+ */
+import java.lang.Math;
+import java.util.Scanner;
+public class Atividade02_Alexandre_e_Felipe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner (System.in);
+        
+        //Questão 1
+//        double raio,area,circunf;
+//       
+//        System.out.println("Digie o valor do raio do circulo:");
+//        
+//        raio = entrada.nextDouble();
+//        area = Math.PI*(Math.pow(raio,2));
+//        circunf = 2*Math.PI*raio;
+//       
+//        System.out.println("Resultado área:"+area);
+//        System.out.println("Resulta circunferência:"+circunf);
+//        
+        //Questão 2
+        
+//        double IMC,kg,m; 
+//        System.out.println("Digite seu peso:");
+//        kg = entrada.nextDouble();
+//        
+//        System.out.println("Digite sua altura:");
+//        m = entrada.nextDouble();
+//        
+//        IMC = kg/(Math.pow(m,2));
+//        System.out.println("Seu IMC e de:"+IMC);        
+//        
+        
+         //Questão 3
+         
+//         double b,a,h,c;
+//         
+//         System.out.println("Digite o primeiro cateto do triangulo:");
+//         b = entrada.nextDouble();
+//         
+//         System.out.println("Digite o segundo cateto do triangulo");
+//         a = entrada.nextDouble();
+//         
+//         c = (Math.pow(b,2)) + (Math.pow(a,2));
+//         
+//         h= Math.pow(c,2);
+//         
+//         System.out.println("O valor da Hipotenusae de:"+h);
+         
+  
+           // Questão 4
+           
+           
+//           double real,dolar,conversao1,conversao2,troco;
+//           System.out.print("Digite a quantidade de reais: ");
+//           real = entrada.nextDouble();
+//           System.out.print("Digite o valor do dolar: ");
+//           dolar = entrada.nextDouble();
+//           conversao1 = real/dolar;
+//           conversao2 = Math.floor(conversao1);
+//           System.out.println("O valor em real convertido da o total de "+conversao2+" dolares");
+//           troco = (conversao1 - conversao2)*dolar;
+//           System.out.println("O troco em reais "+troco);
+
+            // Questão 5
+//
+//           int dado,j1,j2,s;
+//           System.out.print("Digite a quantidade de vezes jogadas: ");
+//           dado = entrada.nextInt();
+//           System.out.print("Digite o numero caido: ");
+//           j1 = entrada.nextInt();
+//           System.out.print("Digite o segundo numero caido: ");
+//           j2 = entrada.nextInt();
+//           s =(j1 + j2)/ dado;
+//           System.out.println("A media e: "+s);
+//           System.out.println("O maior numero foi: "+Math.max(j1,j2));
+//           System.out.println("O menor numero foi: "+Math.min(j1,j2));
+//           
+
+
+            // Questão 6
+//            double angulo,seno,cosseno,tangente,rad;
+//            System.out.print("Digite um angulo: ");
+//            angulo = entrada.nextInt();
+//            rad = Math.toRadians(angulo);
+//            seno = Math.sin(rad);
+//            cosseno = Math.cos(rad);
+//            tangente = Math.tan(rad);
+//            
+//            System.out.println("Seno: "+seno);
+//            System.out.println("Cosseno: "+cosseno);
+//            System.out.println("Tangente: "+tangente);
+//            
+
+
+             // Questão 7
+               
+//             double x1,x2,y1,y2,calc;
+//             System.out.println("Digite o x1:");
+//             x1 = entrada.nextDouble();
+//             System.out.println("Digite o x2:");
+//             x2 = entrada.nextDouble();
+//             System.out.println("Digite o y1:");
+//             y1 = entrada.nextDouble();
+//             System.out.println("Digite o y2:");
+//             y2 = entrada.nextDouble();
+//             calc= Math.sqrt(Math.pow((x2-x1),2)+ (Math.pow((y2-y1),2)));
+//             System.out.print("A distancia da euclidiana e de:"+ calc);
+//             
+            // Questão 8
+//            
+//            double num1,num2,num3,med,soma,dif;
+//            
+//            
+//            System.out.print("Digite o primeiro numero:");
+//            num1 = entrada.nextDouble();
+//            System.out.print("Digite o segundo numero:");
+//            num2 = entrada.nextDouble();
+//            System.out.print("Digite o terceiro numero:");
+//            num3 = entrada.nextDouble();
+//            med = (num1+num2+num3)/3;
+//            System.out.println("A media e de:"+med);
+//            soma = Math.abs(num1)+Math.abs(num2)+Math.abs(num3);
+//            dif = Math.abs(num1-num3);
+//            System.out.println("a diferença e de:"+dif);
+//               
+
+
+           // Questão 9
+           
+//           int a,b,c;
+//           double delta,x1,x2;
+//           
+//           System.out.print("Digite o numero representado por a:");
+//           a = entrada.nextInt();
+//           System.out.print("Digite o numero representado por b:");
+//           b = entrada.nextInt();
+//           System.out.print("Digite o numero representado por c:");
+//           c = entrada.nextInt();
+//           delta = Math.sqrt((Math.pow(b,2))-(4*a*c));
+//           x1 = ((0-b)+delta)/(2*a);
+//           x2 = ((0-b)-delta)/(2*a);
+//           System.out.println("O valor de delta e:"+delta);
+//           System.out.println("x1 linha ="+x1);
+//           System.out.println("x2 linha ="+x2);
+
+
+           //Questão 10
+           
+//           double f,calc;
+//           
+//           System.out.print("Digite a temperatura em Fahrenheit:");
+//           f = entrada.nextDouble();
+//           calc = (f-32)*5/9;
+//           System.out.println("A temperatura em Fahrenheit: "+f+" convertida para Celsios e de: "+calc);
+//           
+           
+           
+             
+             
+             
+             
+    }
+    
+}
