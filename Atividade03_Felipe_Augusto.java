01b35dea1ac7573e34a1ff1ebd05818075c4c3e6
FelipeAugusto1208 <jocenirevangelistarocha@gmail.com>
2025-09-17 21:07:02 -0300

    Add files via upload

---
+++ Atividade03_Felipe_Augusto.java
@@ -0,0 +1,214 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade03_felipe_augusto.java;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade03_Felipe_AugustoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner (System.in);
+        
+        
+        // Questao 1
+        
+//        double acucar;
+//        int porcao,calc;        
+//        System.out.print("Digite quantas porcoes voce deseja: ");
+//        porcao = entrada.nextInt();        
+//        calc = 200/8;
+//        acucar = calc*porcao;
+//        System.out.println("Voce precisa de "+acucar+" gramas de acucar para fazer "+porcao+" porcoes");
+//          
+
+        // Questao 2
+//        
+//          int impressora,cal1,cal2;
+//          
+//          System.out.print("Digite a quantidade de impressoras:");
+//          impressora = entrada.nextInt();
+//          cal1 = 12*3;
+//          cal2 = cal1/impressora;
+//          System.out.println("O tempo utilizado para imprimir e de "+cal2+" minutos utilzando "+impressora+" impressoras, imprimindo as 180 pags.");
+//        
+
+
+        // Questão 3
+        
+//        double km,litro,calc1;
+//        
+//        System.out.print("Digite a distancia em km: ");
+//        km = entrada.nextDouble();
+//        calc1 = 240/18;
+//        litro = km/calc1;
+//        System.out.println("E necessario "+litro+" litros para "+km+" Quilmetros");
+//        
+
+        
+        // Questao 4
+         
+//          int maquina,peca,hora,peca_hr;;
+//          
+//          peca_hr = (900/6)/5;
+//          System.out.print("Digite quantas maquinas irao trabalhar: ");
+//          maquina = entrada.nextInt();
+//          System.out.print("Digite por quantas horas irao trabalhar: ");
+//          hora = entrada.nextInt();
+//          
+//          peca = maquina*(hora*peca_hr);
+//          System.out.println("Vao ser produzidas "+peca+" pecas, por "+maquina+" maquians e por "+hora+" horas");
+//        
+        
+        // Questao 5
+        
+//          double cotacao,dolar,conv;
+//          
+//          System.out.print("Digite a cotacao do dolar: ");
+//          cotacao = entrada.nextDouble();
+//          System.out.print("Digite quanto em dolares deseja comprar: ");
+//          dolar = entrada.nextDouble();
+//          conv = dolar*cotacao;
+//          System.out.println("Voce precisa de "+conv+" reais para comprar "+dolar+" dolares");
+//        
+//        
+
+
+        // Questao 6
+//        
+//          double a,b,x;
+//        
+//          System.out.print("Digite o primeiro numero: ");
+//          a = entrada.nextDouble();
+//          System.out.print("Diite o segundo numero: ");
+//          b = entrada.nextDouble();
+//          x = -b/a;
+//          System.out.println("Valor de a= "+a+"\nValor de b= "+b+"\nValor de x= "+x);
+//        
+
+        // Questao 7
+        
+//          int m,b,x0,y0;
+//          
+//          System.out.print("Digite o valor de m: ");
+//          m = entrada.nextInt();
+//          System.out.print("Digite o valor de b: ");
+//          b = entrada.nextInt();
+//          System.out.print("Digite o valor de x0: ");
+//          x0 = entrada.nextInt();
+//          y0 = m*x0+b;
+//          System.out.println("O valor de y0 = "+y0);
+//          
+
+        // Questao 8
+        
+//          double x1,x2,y1,y2,m,b;
+//          
+//          System.out.print("Digite o valor de x1: ");
+//          x1 = entrada.nextDouble();
+//          System.out.print("Digite o valor de x2: ");
+//          x2 = entrada.nextDouble();
+//          System.out.print("Digite o valor de y1: ");
+//          y1 = entrada.nextDouble();
+//          System.out.print("Digite o valor de y2: ");
+//          y2 = entrada.nextDouble();
+//          
+//          m = (y2-y1)/(x2-x1);
+//          b = y1-m*x1;
+//          System.out.println("y = "+m+" x + "+b);
+
+
+        // Questao 9
+        
+//          double m1,m2,b1,b2,x,y;
+//          
+//          System.out.print("Digite o valor m1: ");
+//          m1= entrada.nextDouble();
+//          System.out.print("Digite o valor m2: ");
+//          m2= entrada.nextDouble();
+//          System.out.print("Digite o valor b1: ");
+//          b1= entrada.nextDouble();
+//          System.out.print("Digite o valor b2: ");
+//          b2= entrada.nextDouble();
+//          x= (b2-b1)/(m1-m2);
+//          y=m1*x+b1;
+//          System.out.println("O valor de x e de: "+x+" e o valor de y e: "+y);
+//          
+          
+        // Questao 10
+        
+//        int a,b,c;
+//        double x1,x2,delta;
+//        System.out.print("Digite o valor de a: ");
+//        a = entrada.nextInt();
+//        System.out.print("Digite o valor de b: ");
+//        b = entrada.nextInt();
+//        System.out.print("O valor de c: ");
+//        c = entrada.nextInt();
+//        delta = Math.pow(b,2)-(4*a*c);
+//        x1 = (-b + Math.sqrt(delta))/(2 *a);
+//        x2 = (-b - Math.sqrt(delta))/(2*a);
+//        System.out.print("Valor de x1: "+x1);
+//        System.out.println("Valor de x2: "+x2);
+//        System.out.println("O valor de delta e: "+delta);
+        
+
+        // Questao 11
+        
+//        int a,b,c;;
+//        double delta,d;
+//        System.out.print("Digite o valor de a: ");
+//        a = entrada.nextInt();
+//        System.out.print("Digite o valor de b: ");
+//        b = entrada.nextInt();
+//        System.out.print("O valor de c: ");
+//        c = entrada.nextInt();
+//        delta = Math.pow(b,2)-(4*a*c);
+//        d = Math.abs((Math.sqrt(delta))/a);
+//        System.out.println("O valor da distancia e: "+d);
+//        
+
+        // Questao 12
+        
+//        int a,b,c,x0;
+//        double f,calc2;
+//        System.out.println("Digite  valor de a: ");
+//        a = entrada.nextInt();
+//        System.out.print("Digite o valor de b: ");
+//        b = entrada.nextInt();
+//        System.out.print("O valor de c: ");
+//        c = entrada.nextInt();
+//        System.out.println("Digite o valo x0: ");
+//        x0 = entrada.nextInt();
+//        f = a*(Math.pow(x0,2))+b*x0+c;
+//        calc2 = 2*a*x0+b;
+//        System.out.println("O valor no primero ponto e de: "+f+" e o valor do segundo ponto e de: "+calc2);
+//        
+        
+        // Questao 13
+        
+//        double m,b,x1,x2,y1,y2,A;
+//        
+//        System.out.println("Digite  valor de m: ");
+//        m = entrada.nextDouble();
+//        System.out.print("Digite o valor de b: ");
+//        b = entrada.nextDouble();
+//        System.out.print("O valor de x1: ");
+//        x1 = entrada.nextDouble();
+//        System.out.println("Digite o valo x2: ");
+//        x2 = entrada.nextDouble();
+//        y1 = m*x1+b;
+//        y2 = m*x2+b;
+//        A =  ((y1+y2)/2)*(x2-x1);
+//        System.out.print("O valoe da Area e de: "+A);
+    }
+    
+}
