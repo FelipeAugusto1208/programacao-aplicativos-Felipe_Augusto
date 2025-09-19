8655f493af01252563fec61fe49abadc7708f55d
FelipeAugusto1208 <jocenirevangelistarocha@gmail.com>
2025-09-18 21:00:41 -0300

    Add files via upload

---
+++ Atividade04_Felipe_Augusto.java
@@ -0,0 +1,295 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade04_felipe_augusto;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Atividade04_Felipe_Augusto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        
+        // Questao 1
+        
+//        int num;
+//        
+//        System.out.print("Digite um numero: ");
+//        num = entrada.nextInt();
+//         if (num < 0) {
+//         System.out.println("Voce digitou um numero negativo");}
+//         else {
+//         System.out.println("Voce digitou um numero positivo");}
+//        
+        
+        // Questao 2
+        
+//        int num;
+//        
+//        System.out.print("Digite um numero: ");
+//        num = entrada.nextInt();
+//        if (num < 0){
+//        System.out.println("Voce digitou um numero negativo");}
+//        else {if (num > 0) {
+//        System.out.println("Voce digitou um numero positivo");}
+//        else {
+//        System.out.println("Voce digitou zero");}}
+//        
+        
+        // Questao 3
+        
+//        double nota1,nota2,nota3,media;
+//        
+//        System.out.print("Digite a primeira nota: ");
+//        nota1 = entrada.nextDouble();
+//        System.out.print("Digite a segunda nota: ");
+//        nota2 = entrada.nextDouble();
+//        System.out.print("Digite a terceira nota: ");
+//        nota3 = entrada.nextDouble();
+//        
+//        media = (nota1+nota2+nota3)/3;
+//        
+//        if (media >= 7) {
+//        System.out.println("Aluno Aprovado!\nSua media foi "+media);}
+//        else {
+//        System.out.println("Aluno Reprovado!\nSua media foi "+media);}
+//        
+        
+        // Questao 4
+        
+//        double nota1,nota2,nota3,media;
+//        
+//        System.out.print("Digite a primeira nota: ");
+//        nota1 = entrada.nextDouble();
+//        System.out.print("Digite a segunda nota: ");
+//        nota2 = entrada.nextDouble();
+//        System.out.print("Digite a terceira nota: ");
+//        nota3 = entrada.nextDouble();
+//        
+//        media = (nota1+nota2+nota3)/3;
+//        
+//        if (media >= 7) {
+//        System.out.println("Aluno Aprovado!\nSua media foi "+media);}
+//        else { if (media >= 5 && media < 7) {
+//        System.out.println("Aluno em Recuperacao!\nSua media foi "+media);}
+//        else{
+//        System.out.println("Aluno Reprovado!\nSua media foi "+media);}}
+//        
+        
+        // Questao 5
+        
+//          int num;
+//          
+//          System.out.print("Digite um numero: ");
+//          num = entrada.nextInt();
+//          if (num % 2 == 0){
+//          System.out.println("Numero par");}
+//          else {
+//          System.out.println("Numero impar");}
+//          
+          
+        // Questao 6
+          
+//          int num1,num2,num3;
+//            
+//          System.out.print("Digite o primeiro numero: ");
+//          num1 = entrada.nextInt();
+//          System.out.print("Digite o segundo numero: ");
+//          num2 = entrada.nextInt();
+//          System.out.print("Digite o terceiro numero: ");
+//          num3 = entrada.nextInt();
+//          
+//          if (num1 > num2 && num1 > num3) {
+//          System.out.println("O primeiro numero e o maior "+num1);}
+//          else {
+//          if (num2 > num3) {
+//          System.out.println("O segundo numero e o maior "+num2);}
+//          else {
+//          System.out.println("O terceiro numero e o maior "+num3);}}
+//          
+          
+          // Questao 7
+//          
+//            int idade;
+//            
+//            System.out.print("Digite sua idade: ");
+//            idade = entrada.nextInt();
+//            
+//            if (idade >= 18) {
+//            System.out.println("Adulto");}
+//            else {
+//            if (idade >=14) {
+//            System.out.println("Juvenil");}
+//            else {
+//            if (idade >= 9){
+//            System.out.println("Infantil");}
+//            else{
+//            System.out.println("Mirin");}}}
+//            
+            
+            // Questao 8
+            
+//            int num,soma,sub;
+//            
+//            System.out.print("Digite um numero: ");
+//            num = entrada.nextInt();
+//            if (num > 20){
+//            soma = num + 8;
+//            System.out.println("O valor da soma e de: "+soma);}
+//            else{
+//            sub = num - 5;
+//            System.out.println("O valor da subtracao e de: "+sub);}
+//           
+
+            // Questao 9
+            
+            
+//            double valor,lucro,venda;
+//            
+//            System.out.print("Digite o valor da compra: ");
+//            valor = entrada.nextDouble();
+//            if (valor < 20){
+//            lucro = (valor*45)/100;
+//            venda = lucro + valor;
+//            System.out.println("O valor da venda e de: "+venda);
+//            }
+//            else{
+//            lucro = (valor*30)/100;
+//            venda = lucro + valor;
+//            System.out.println("O valor da venda e de: "+venda);}
+                   
+          //Questao 10
+          
+//          double calc;
+//          int maca;
+//          
+//          System.out.print("Digite a quantidade de maca: ");
+//          maca = entrada.nextInt();
+//          if (maca >= 12){
+//          calc = maca * 0.25;
+//          System.out.println("O valor das macas ira sair a: "+calc);}
+//          else{
+//          calc = maca * 0.30;
+//          System.out.println("O valor das macas vai sair a: "+calc);}
+//          
+//
+
+          //Questao 11
+          
+//          int sexo;
+//          double altura,calc;
+//          
+//          System.out.print("Digite sua altura: ");
+//          altura = entrada.nextDouble();
+//          System.out.println("Digite 1 para Feminino e 2 para Masculino");
+//          System.out.print("Digite o numero correspondente para seu sexo: ");
+//          sexo = entrada.nextInt();
+//          if(sexo == 1){
+//          calc = (62.1*altura)-44.7;
+//          System.out.println("O seu peso ideal e de "+calc+" kg");}
+//          else{
+//          calc = (72.7*altura)-58;
+//          System.out.println("O seu peso ideal e de "+calc+" kg");}
+//     
+
+          //Questao 12
+          
+//          int dia;
+//          System.out.println("Iniciase no Domingo sendo dia 1 e finaliza no sabado sendo o dia 7.");
+//          System.out.print("Escreva um numero correspondenete ao dia da semana: ");
+//          dia = entrada.nextInt();
+//          
+//          switch (dia) {
+//              case 1: System.out.println("Domingo: Final de Semana");
+//              break;
+//              case 2: System.out.println("Segunda: Dia Util");
+//              break;
+//              case 3: System.out.println("Terca: Dia Util");
+//              break;
+//              case 4: System.out.println("Quarta: Dia Util");
+//              break;
+//              case 5: System.out.println("Quinta: Dia Util");
+//              break;
+//              case 6: System.out.println("Sexta: Dia Util");
+//              break;
+//              case 7: System.out.println("Sabado: Final de Semana");
+//              break;
+//              default: System.out.println("Numero de dia invalido");
+//          }
+              
+          
+          // Questao 13
+          
+//          int cod_prod;
+//          double soma;
+//          
+//          System.out.println(
+//                  "Codigo:100 |Produto: Detergente|Valor unitario: $1.59\n"
+//                 +"Codigo:101 |Produto: Espoja|Valor unitario: $4.59\n"
+//                 +"Codigo:102 |Produto: La de aco|Valor unitario: $1.79");
+//          System.out.print("Digite o codigo o produto: ");
+//          cod_prod = entrada.nextInt();
+//          soma = 0.0;
+//          switch(cod_prod){
+//              case 100:
+//                  System.out.print("Detergente - R$1.59");
+//                  soma = soma + 1.59;
+//                  break;
+//              case 101:
+//                  System.out.print("Espoja - R$4.59");
+//                  soma = soma + 4.59;
+//                  break;
+//              case 102:
+//                  System.out.print("La de aco - R$1.79");
+//                  soma = soma + 1.79;
+//                  break;
+//              default:
+//                  System.out.print("Codigo invalido.");
+          
+          }
+
+
+
+            // Questao 14
+            
+//            double num1,num2,soma,sub,mult,div;
+//            int operador;
+//            
+//            System.out.println("Soma: 1 \nSubtracao: 2 \nMultiplicacao: 3 \nDivisao: 4");
+//            System.out.print("Digite qual operacao deseja fazer: ");
+//            operador = entrada.nextInt();
+//            System.out.print("Digite o primeiro numero: ");
+//            num1 = entrada.nextDouble();
+//            System.out.print("Digite o segundo numero: ");
+//            num2 = entrada.nextDouble();
+//            
+//            switch (operador){
+//                case 1: soma = num1+num2;
+//                System.out.println("Soma: "+soma);
+//                break;
+//                case 2: sub = num1-num2;
+//                System.out.println("Subtracao: "+sub);
+//                break;
+//                case 3: mult = num1*num2;
+//                System.out.println("Multiplicacao: "+mult);
+//                break;
+//                case 4: div = num1/num2;
+//                System.out.println("Divisao: "+div);
+//                break;   
+//            }
+            
+          
+          
+    }
+    
+}
